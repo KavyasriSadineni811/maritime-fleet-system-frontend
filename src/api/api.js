1ed27328d1d0f50fae2f261ca0eea7736328bc53
Kavyasri Sadineni <23jr1a43e3@gmail.com>
2026-01-12 10:53:21 +0530

    Update backend URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",  // backend url
+  baseURL: "https://maritime-fleet-system-backend-4.onrender.com",  // backend url
 });
 
 // for sending token automatically
